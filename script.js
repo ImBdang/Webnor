7b97372c70c2ab10439d50dcd94905ff63a4db76
Imbdang <dtc225200211@ictu.edu.vn>
2025-07-27 05:11:07 +0700

    thay url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const provinces_api = "https://cmn5rowwiwusexidep6c62c5mu0dsjbt.lambda-url.ap-southeast-2.on.aws/"
-const gemini_api = "https://3wsknos7fhh6w55o5vkqoruogy0brnvr.lambda-url.ap-southeast-2.on.aws/"
+const provinces_api = "https://kzvqmshdea3rkoaajk3u73bh4a0msndx.lambda-url.ap-southeast-2.on.aws/"
+const gemini_api = "https://5rhies7unl5ketoemsi6xs53oq0ocnrh.lambda-url.ap-southeast-2.on.aws/"
 
 async function get_provinces(code){
     const params = new URLSearchParams({
